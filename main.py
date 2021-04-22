1a0e587a5fb9353a9fca6151981b1e35e51d6ebf
Dracevui <liam_aumento@yahoo.com>
2021-04-22 20:28:52 +0100

    Drew block onto the game window and got it moving via arrow keys

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def game_quit():
 
 
 def scale_window():  # Scales the game window and assets to fit the user's monitor dimensions
+    DUMMY_WINDOW.fill(MAGENTA)
+    DUMMY_WINDOW.blit(block, (block_x, block_y))
     frame = pygame.transform.scale(DUMMY_WINDOW, SCREEN_DIMENSIONS)
     WINDOW.blit(frame, frame.get_rect())
     pygame.display.flip()
@@ -30,19 +32,29 @@ if __name__ == "__main__":
     WHITE = (255, 255, 255)
     MAGENTA = (142, 63, 255)
 
-    DUMMY_WINDOW.fill(MAGENTA)
-    scale_window()
-
     # Asset Files
     block = pygame.image.load("resources/block.jpg").convert()
+    block_x = 100
+    block_y = 100
 
     # Game Variables
     running = True
 
     while running:
+        scale_window()
         for event in pygame.event.get():
             if event.type == KEYDOWN and event.key == K_ESCAPE:
                 running = False
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    block_y -= 10
+                if event.key == K_DOWN:
+                    block_y += 10
+                if event.key == K_LEFT:
+                    block_x -= 10
+                if event.key == K_RIGHT:
+                    block_x += 10
+                scale_window()
             if event.type == QUIT:
                 running = False
                 game_quit()
