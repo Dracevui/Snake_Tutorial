2a9dbb328263e6e0f11d8ca7cce96214253aca00
Dracevui <liam_aumento@yahoo.com>
2021-04-22 21:07:01 +0100

    Converted program to follow OOP principles

--- main.py
+++ main.py
@@ -1,60 +1,85 @@
 import pygame
-from pygame.locals import *
 import sys
 import math
 
 
-def game_quit():
-    pygame.quit()
-    sys.exit()
+class Snake:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.block = pygame.image.load("resources/block.jpg").convert()
+        self.x = 100
+        self.y = 100
+        self.WHITE = (255, 255, 255)
+        self.MAGENTA = (142, 63, 255)
+
+    def move_left(self):
+        self.x -= 10
+        self.draw()
+
+    def move_right(self):
+        self.x += 10
+        self.draw()
+
+    def move_up(self):
+        self.y -= 10
+        self.draw()
+
+    def move_down(self):
+        self.y += 10
+        self.draw()
+
+    def draw(self):
+        self.parent_screen.fill(self.MAGENTA)
+        self.parent_screen.blit(self.block, (self.x, self.y))
+        pygame.display.flip()
 
 
-def scale_window():  # Scales the game window and assets to fit the user's monitor dimensions
-    DUMMY_WINDOW.fill(MAGENTA)
-    DUMMY_WINDOW.blit(block, (block_x, block_y))
-    frame = pygame.transform.scale(DUMMY_WINDOW, SCREEN_DIMENSIONS)
-    WINDOW.blit(frame, frame.get_rect())
-    pygame.display.flip()
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.MONITOR = pygame.display.Info()
+        self.SCREEN_DIMENSIONS = (math.ceil(self.MONITOR.current_w * 0.26), math.floor(self.MONITOR.current_h * 0.463))
+        self.WINDOW = pygame.display.set_mode(self.SCREEN_DIMENSIONS)
+        self.DUMMY_WINDOW = pygame.Surface((500, 500))
+        self.WIDTH, HEIGHT = self.SCREEN_DIMENSIONS
+        self.WINDOW_WIDTH = self.WINDOW.get_width()
+        self.WINDOW_HEIGHT = self.WINDOW.get_height()
+
+        self.snake = Snake(self.DUMMY_WINDOW)
+        self.snake.draw()
+
+    def scale_window(self):  # Scales the game window and assets to fit the user's monitor dimensions
+        frame = pygame.transform.scale(self.DUMMY_WINDOW, self.SCREEN_DIMENSIONS)
+        self.WINDOW.blit(frame, frame.get_rect())
+        pygame.display.flip()
+
+    @staticmethod
+    def game_quit():
+        pygame.quit()
+        sys.exit()
+
+    def run(self):
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.snake.move_up()
+                    if event.key == pygame.K_DOWN:
+                        self.snake.move_down()
+                    if event.key == pygame.K_LEFT:
+                        self.snake.move_left()
+                    if event.key == pygame.K_RIGHT:
+                        self.snake.move_right()
+                if event.type == pygame.QUIT:
+                    running = False
+                    self.game_quit()
+            self.scale_window()
 
 
 if __name__ == "__main__":
-    # Constants
-    pygame.init()
-    MONITOR = pygame.display.Info()
-    SCREEN_DIMENSIONS = (math.ceil(MONITOR.current_w * 0.26), math.floor(MONITOR.current_h * 0.463))
-    WINDOW = pygame.display.set_mode(SCREEN_DIMENSIONS)
-    DUMMY_WINDOW = pygame.Surface((500, 500))
-    WIDTH, HEIGHT = SCREEN_DIMENSIONS
-    WINDOW_WIDTH = WINDOW.get_width()
-    WINDOW_HEIGHT = WINDOW.get_height()
-
-    # Colours
-    WHITE = (255, 255, 255)
-    MAGENTA = (142, 63, 255)
-
-    # Asset Files
-    block = pygame.image.load("resources/block.jpg").convert()
-    block_x = 100
-    block_y = 100
-
-    # Game Variables
-    running = True
-
-    while running:
-        scale_window()
-        for event in pygame.event.get():
-            if event.type == KEYDOWN and event.key == K_ESCAPE:
-                running = False
-            if event.type == KEYDOWN:
-                if event.key == K_UP:
-                    block_y -= 10
-                if event.key == K_DOWN:
-                    block_y += 10
-                if event.key == K_LEFT:
-                    block_x -= 10
-                if event.key == K_RIGHT:
-                    block_x += 10
-                scale_window()
-            if event.type == QUIT:
-                running = False
-                game_quit()
+    game = Game()
+    game.run()
