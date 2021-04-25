e6a4f1de2ccde502efb391c62bbd6ee0605a8335
Dracevui <liam_aumento@yahoo.com>
2021-04-25 13:47:35 +0100

    Added comments to all my functions and classes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 SIZE = 40
 
 
-def screen_dimensions(x, y):
+def screen_dimensions(x, y):  # Lets me easily change the dimensions of the game window
     monitor = pygame.display.Info()
     base_x, base_y = 1920, 1080
     target_x = x / base_x
@@ -15,7 +15,7 @@ def screen_dimensions(x, y):
     return final_x, final_y
 
 
-def update_score(score, hi_score):
+def update_score(score, hi_score):  # Replaces the high score if the current score surpasses the current one
     if score > hi_score:
         hi_score = score
     return hi_score
@@ -29,39 +29,42 @@ class Apple:
         self.x = SIZE * 3
         self.y = SIZE * 3
 
-    def draw(self):
+    def draw(self):  # Draws the apple onscreen
         self.parent_screen.blit(self.image, (self.x, self.y))
         pygame.display.flip()
 
-    def move(self):
+    def move(self):  # Handles moving the apple once it has been eaten
         self.x = random.randint(0, 24) * SIZE
         self.y = random.randint(0, 19) * SIZE
 
-    def play_sound(self):
+    def play_sound(self):  # Plays the 'crunch' sound effect once it has been eaten
         pygame.mixer.Channel(1).play(self.crunch)
 
 
 class Snake:
     def __init__(self, parent_screen, length):
+        # Game Variables
         self.length = length
         self.parent_screen = parent_screen
         self.block = pygame.image.load("resources/block.jpg").convert_alpha()
         self.x = [SIZE] * length
         self.y = [SIZE] * length
-        self.WHITE = (255, 255, 255)
-        self.MAGENTA = (142, 63, 255)
         self.direction = ""
         self.north = False
-        self.south = True
+        self.south = False
         self.east = False
         self.west = False
 
-    def increase_length(self):
+        # Colours
+        self.WHITE = (255, 255, 255)
+        self.MAGENTA = (142, 63, 255)
+
+    def increase_length(self):  # Increases the length of the snake
         self.length += 1
         self.x.append(-1)
         self.y.append(-1)
 
-    def move_left(self):
+    def move_left(self):  # Turns the snake left
         if not self.east:
             self.direction = "left"
             self.north = False
@@ -69,7 +72,7 @@ class Snake:
             self.east = False
             self.west = True
 
-    def move_right(self):
+    def move_right(self):  # Turns the snake right
         if not self.west:
             self.direction = "right"
             self.north = False
@@ -77,7 +80,7 @@ class Snake:
             self.east = True
             self.west = False
 
-    def move_up(self):
+    def move_up(self):  # Turns the snake up
         if not self.south:
             self.direction = "up"
             self.north = True
@@ -85,7 +88,7 @@ class Snake:
             self.east = False
             self.west = False
 
-    def move_down(self):
+    def move_down(self):  # Turns the snake down
         if not self.north:
             self.direction = "down"
             self.north = False
@@ -93,12 +96,12 @@ class Snake:
             self.east = False
             self.west = False
 
-    def draw(self):
+    def draw(self):  # Draws the snake onscreen
         for i in range(self.length):
             self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
         pygame.display.flip()
 
-    def walk(self):
+    def walk(self):  # Handles the continuous motion of the snake once input direction is detected
         for i in range(self.length-1, 0, -1):
             self.x[i] = self.x[i - 1]
             self.y[i] = self.y[i - 1]
@@ -115,7 +118,7 @@ class Snake:
         self.draw()
 
 
-class Assets:
+class Assets:  # The class that handles loading in game assets
     def __init__(self):
         self.background = pygame.transform.scale((pygame.image.load("resources/water_background.png")), (1000, 800))
         self.game_over = pygame.image.load("resources/game_over.png")
@@ -123,7 +126,7 @@ class Assets:
         self.icon = pygame.image.load("resources/icon.png")
         self.bgm = pygame.mixer.Sound("resources/bgm.wav")
 
-    def play_bgm(self):
+    def play_bgm(self):  # Plays the background music
         self.bgm.play(-1)
 
 
@@ -161,7 +164,7 @@ class Game:
         self.snake.draw()
         self.apple.draw()
 
-    def game_over(self):
+    def game_over(self):  # Draws the game over screen when you die
         self.game_active = False
         while not self.game_active:
             for event in pygame.event.get():
@@ -177,11 +180,15 @@ class Game:
             self.display_score()
             self.scale_window()
 
-    def game_clear(self):
+    def game_clear(self):  # Clears all the relevant variables to start a new game session
         self.snake.length = 1
         self.snake.direction = ""
+        self.snake.north = False
+        self.snake.west = False
+        self.snake.east = False
+        self.snake.west = False
 
-    def play(self):
+    def play(self):  # Handles the logic of when a game session is in play
         self.DUMMY_WINDOW.blit(self.assets.background, (0, 0))
         self.snake.walk()
         
@@ -197,7 +204,7 @@ class Game:
         self.apple.draw()
         pygame.display.flip()
 
-    def check_collision(self, x1, y1, x2, y2):
+    def check_collision(self, x1, y1, x2, y2):  # Handles collision logic
         if x2 <= x1 < x2 + SIZE and y2 <= y1 < y2 + SIZE:
             self.apple.play_sound()
             return True
@@ -209,11 +216,11 @@ class Game:
         pygame.display.flip()
 
     @staticmethod
-    def game_quit():
+    def game_quit():  # Quits the game when prompted
         pygame.quit()
         sys.exit()
 
-    def display_score(self):
+    def display_score(self):  # Displays the score and high score onto the screen
         score = self.FONT.render(f"Score: {self.snake.length - 1}", True, self.WHITE)
         hi_score = self.FONT.render(f"High Score: {self.high_score}", True, self.WHITE)
         
@@ -223,7 +230,7 @@ class Game:
         if not self.game_active:
             self.DUMMY_WINDOW.blit(hi_score, (365, 690))
 
-    def run(self):
+    def run(self):  # Contains the main running loop
         self.assets.play_bgm()
         while self.running:
             for event in pygame.event.get():
